5d30149ae851aace976c5713cebed53a03169181
Adam Pinner <hooklock@gmail.com>
2016-06-13 20:43:26 +0100

    initial commit, file structure, empty files

---
+++ webpack_record_store/client/src/app.js
@@ -0,0 +1,7 @@
+var Store = require("./record-store/record_store.js");
+var Record = require("./record-store/record.js");
+var Collector = require("./record-store/record_collector.js");
+
+window.onload = function(){
+  console.log("App created from webpack");
+};
